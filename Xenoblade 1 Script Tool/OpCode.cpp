c5e09c9edffeb2d3ab0eaae5560ea68763d1b7e1
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-08-13 16:44:42 -0500

    Added OpCode class
    OpCode hold the opcode value of an instruction, and provides information about each opcode: string representation, expected operand size in bytes

---
+++ Xenoblade 1 Script Tool/OpCode.cpp
@@ -0,0 +1,235 @@
+#include "OpCode.h"
+#include <unordered_map>
+#include <string>
+#include <stdexcept>
+
+const std::string OpCode::OpCodeStrings[] = { 
+    "NOP",
+    "CONST_0",
+    "CONST_1",
+    "CONST_2",
+    "CONST_3",
+    "CONST_4",
+    "CONST_I",
+    "CONST_I_W",
+    "POOL_INT",
+    "POOL_INT_W",
+    "POOL_FIXED",
+    "POOL_FIXED_W",
+    "POOL_STR",
+    "POOL_STR_W",
+    "LD",
+    "ST",
+    "LD_ARG",
+    "ST_ARG",
+    "ST_ARG_OMIT",
+    "LD_0",
+    "LD_1",
+    "LD_2",
+    "LD_3",
+    "ST_0",
+    "ST_1",
+    "ST_2",
+    "ST_3",
+    "LD_ARG_0",
+    "LD_ARG_1",
+    "LD_ARG_2",
+    "LD_ARG_3",
+    "ST_ARG_0",
+    "ST_ARG_1",
+    "ST_ARG_2",
+    "ST_ARG_3",
+    "LD_STATIC",
+    "LD_STATIC_W",
+    "ST_STATIC",
+    "ST_STATIC_W",
+    "LD_AR",
+    "ST_AR",
+    "LD_NIL",
+    "LD_TRUE",
+    "LD_FALSE",
+    "LD_FUNC",
+    "LD_FUNC_W",
+    "LD_PLUGIN",
+    "LD_PLUGIN_W",
+    "LD_FUNC_FAR",
+    "LD_FUNC_FAR_W",
+    "MINUS",
+    "NOT",
+    "L_NOT",
+    "ADD",
+    "SUB",
+    "MUL",
+    "DIV",
+    "MOD",
+    "OR",
+    "AND",
+    "R_SHIFT",
+    "L_SHIFT",
+    "EQ",
+    "NE",
+    "GT",
+    "LT",
+    "GE",
+    "LE",
+    "L_OR",
+    "L_AND",
+    "JMP",
+    "JPF",
+    "CALL",
+    "CALL_W",
+    "CALL_IND",
+    "RET",
+    "NEXT",
+    "PLUGIN",
+    "PLUGIN_W",
+    "CALL_FAR",
+    "CALL_FAR_W",
+    "GET_OC",
+    "GET_OC_W",
+    "GETTER",
+    "GETTER_W",
+    "SETTER",
+    "SETTER_W",
+    "SEND",
+    "SEND_W",
+    "TYPEOF",
+    "SIZEOF",
+    "SWITCH",
+    "INC",
+    "DEC",
+    "EXIT",
+    "BP"
+};
+
+// Expected operand size in bytes
+const unsigned int OpCode::OperandSizes[] = {
+    0, // NOP
+    0, // CONST_0
+    0, // CONST_1
+    0, // CONST_2
+    0, // CONST_3
+    0, // CONST_4
+    1, // CONST_I
+    2, // CONST_I_W
+    1, // POOL_INT
+    2, // POOL_INT_W
+    1, // POOL_FIXED
+    2, // POOL_FIXED_W
+    1, // POOL_STR
+    2, // POOL_STR_W
+    1, // LD
+    1, // ST
+    1, // LD_ARG
+    1, // ST_ARG
+    1, // ST_ARG_OMIT
+    0, // LD_0
+    0, // LD_1
+    0, // LD_2
+    0, // LD_3
+    0, // ST_0
+    0, // ST_1
+    0, // ST_2
+    0, // ST_3
+    0, // LD_ARG_0
+    0, // LD_ARG_1
+    0, // LD_ARG_2
+    0, // LD_ARG_3
+    0, // ST_ARG_0
+    0, // ST_ARG_1
+    0, // ST_ARG_2
+    0, // ST_ARG_3
+    1, // LD_STATIC
+    2, // LD_STATIC_W
+    1, // ST_STATIC
+    2, // ST_STATIC_W
+    0, // LD_AR
+    0, // ST_AR
+    0, // LD_NIL
+    0, // LD_TRUE
+    0, // LD_FALSE
+    1, // LD_FUNC
+    2, // LD_FUNC_W
+    1, // LD_PLUGIN
+    2, // LD_PLUGIN_W
+    1, // LD_FUNC_FAR
+    2, // LD_FUNC_FAR_W
+    0, // MINUS
+    0, // NOT
+    0, // L_NOT
+    0, // ADD
+    0, // SUB
+    0, // MUL
+    0, // DIV
+    0, // MOD
+    0, // OR
+    0, // AND
+    0, // R_SHIFT
+    0, // L_SHIFT
+    0, // EQ
+    0, // NE
+    0, // GT
+    0, // LT
+    0, // GE
+    0, // LE
+    0, // L_OR
+    0, // L_AND
+    2, // JMP
+    2, // JPF
+    1, // CALL
+    2, // CALL_W
+    0, // CALL_IND
+    0, // RET
+    0, // NEXT
+    1, // PLUGIN
+    2, // PLUGIN_W
+    1, // CALL_FAR
+    2, // CALL_FAR_W
+    1, // GET_OC
+    2, // GET_OC_W
+    1, // GETTER
+    2, // GETTER_W
+    1, // SETTER
+    2, // SETTER_W
+    1, // SEND
+    2, // SEND_W
+    0, // TYPEOF
+    0, // SIZEOF
+    1, // SWITCH
+    0, // INC
+    0, // DEC
+    0, // EXIT
+    0, // BP
+};
+
+OpCode::OpCode(unsigned int opcodeVal)
+{
+	this->opcodeVal = opcodeVal;
+}
+
+OpCode::OpCode(std::string opcodeString)
+{
+    this->opcodeVal = -1;
+    for (int i = 0; i < 96; i++) {
+        if (strcmp(OpCodeStrings[i].c_str(), opcodeString.c_str())) {
+            this->opcodeVal = i;
+            break;
+        }
+    }
+    if (this->opcodeVal == -1) throw std::runtime_error(opcodeString + " is not a valid opcode");
+}
+
+unsigned int OpCode::getOperandSize()
+{
+    return OperandSizes[this->opcodeVal];
+}
+
+std::string OpCode::getOpCodeString()
+{
+    return OpCodeStrings[this->opcodeVal];
+}
+
+unsigned int OpCode::getOpCodeVal()
+{
+    return this->opcodeVal;
+}
---
+++ Xenoblade 1 Script Tool/OpCode.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <string>
+
+class OpCode
+{
+	unsigned int opcodeVal;
+
+	static const std::string OpCodeStrings[96];
+	static const unsigned int OperandSizes[96];
+
+public:
+	OpCode(unsigned int opcodeVal);
+	OpCode(std::string opcode);
+	OpCode() = default;
+
+    static const enum OpCodes {
+        NOP,
+        CONST_0,
+        CONST_1,
+        CONST_2,
+        CONST_3,
+        CONST_4,
+        CONST_I,
+        CONST_I_W,
+        POOL_INT,
+        POOL_INT_W,
+        POOL_FIXED,
+        POOL_FIXED_W,
+        POOL_STR,
+        POOL_STR_W,
+        LD,
+        ST,
+        LD_ARG,
+        ST_ARG,
+        ST_ARG_OMIT,
+        LD_0,
+        LD_1,
+        LD_2,
+        LD_3,
+        ST_0,
+        ST_1,
+        ST_2,
+        ST_3,
+        LD_ARG_0,
+        LD_ARG_1,
+        LD_ARG_2,
+        LD_ARG_3,
+        ST_ARG_0,
+        ST_ARG_1,
+        ST_ARG_2,
+        ST_ARG_3,
+        LD_STATIC,
+        LD_STATIC_W,
+        ST_STATIC,
+        ST_STATIC_W,
+        LD_AR,
+        ST_AR,
+        LD_NIL,
+        LD_TRUE,
+        LD_FALSE,
+        LD_FUNC,
+        LD_FUNC_W,
+        LD_PLUGIN,
+        LD_PLUGIN_W,
+        LD_FUNC_FAR,
+        LD_FUNC_FAR_W,
+        MINUS,
+        NOT,
+        L_NOT,
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        MOD,
+        OR,
+        AND,
+        R_SHIFT,
+        L_SHIFT,
+        EQ,
+        NE,
+        GT,
+        LT,
+        GE,
+        LE,
+        L_OR,
+        L_AND,
+        JMP,
+        JPF,
+        CALL,
+        CALL_W,
+        CALL_IND,
+        RET,
+        NEXT,
+        PLUGIN,
+        PLUGIN_W,
+        CALL_FAR,
+        CALL_FAR_W,
+        GET_OC,
+        GET_OC_W,
+        GETTER,
+        GETTER_W,
+        SETTER,
+        SETTER_W,
+        SEND,
+        SEND_W,
+        TYPEOF,
+        SIZEOF,
+        SWITCH,
+        INC,
+        DEC,
+        EXIT,
+        BP
+    };
+
+	unsigned int getOperandSize();
+	std::string getOpCodeString();
+	unsigned int getOpCodeVal();
+};
+
