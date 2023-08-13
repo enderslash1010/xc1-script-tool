2171c81e36c36471f3170429b9ca92fd4ce36b7f
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-08-13 16:51:10 -0500

    Removed using namespace std and getting started comments

--- Xenoblade 1 Script Tool/Xenoblade 1 Script Tool.cpp
+++ Xenoblade 1 Script Tool/Xenoblade 1 Script Tool.cpp
@@ -1,29 +1,16 @@
-// Xenoblade 1 Script Tool.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
 #include "Script.h"
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
-    if (argc != 2) cout << "Usage: " << argv[0] << " <fileName>" << endl;
+    if (argc != 2) std::cout << "Usage: " << argv[0] << " <fileName>" << std::endl;
     else {
         try {
             Script script(argv[1]);
         }
-        catch (exception& e) {
-            cout << e.what() << endl;
+        catch (std::exception& e) {
+            std::cout << e.what() << std::endl;
         }
     }
 }
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
