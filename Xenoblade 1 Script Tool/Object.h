7c15866af5bf7c686dfa730717e1ed5f2a42fa68
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-08-21 21:31:20 -0500

    Added Type enum and attribute to Object

--- Xenoblade 1 Script Tool/Object.h
+++ Xenoblade 1 Script Tool/Object.h
@@ -3,15 +3,23 @@
 
 class Object
 {
+public:
+	enum Type {
+		Null, True, False, Int, Fixed, String, Array, Function, Plugin, OC, Sys
+	};
+
+private:
 	std::string type;
+	Type typeEnum;
 	unsigned int length;
 	unsigned int value;
 	unsigned int field8; // only in 64-bit scripts, for pointer storage space
 
 public:
-	Object(std::string type, unsigned int length, unsigned int value, unsigned int field8);
+	Object(Type type, unsigned int length, unsigned int value, unsigned int field8);
 
 	std::string getType();
+	Type getTypeEnum();
 	unsigned int getLength(), getValue(), getField8();
 };
 
