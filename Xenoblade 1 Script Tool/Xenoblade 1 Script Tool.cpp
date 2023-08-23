fc4c8fbf83de8d0d4db491f6adba70fa11be6f4d
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-08-23 17:33:40 -0500

    Minor Changes
    - put try/catch around everything in main
    - added 'test' usage to run tests to see if input scripts create output scripts that are identical (and they do now)

--- Xenoblade 1 Script Tool/Xenoblade 1 Script Tool.cpp
+++ Xenoblade 1 Script Tool/Xenoblade 1 Script Tool.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "Script.h"
+#include "Tests.h"
 
 void printUsageStatement(std::string progName) 
 {
@@ -14,20 +15,24 @@ int main(int argc, char *argv[])
     /* Generate Output File: <prog_name> decompile <input_script> <output_csv>
        Generate Script File: <prog_name> compile <input_csv> <output_script>
     */
-    if (argc != 4) printUsageStatement(argv[0]);
-    else {
-        if (strcmp(argv[1], "decompile") == 0) {
-            try {
+    try {
+        if (argc != 4) printUsageStatement(argv[0]);
+        else {
+            if (strcmp(argv[1], "decompile") == 0) {
                 Script script(argv[2]);
                 script.generateOutfile(argv[3]);
             }
-            catch (std::exception& e) {
-                std::cout << e.what() << std::endl;
+            else if (strcmp(argv[1], "compile") == 0) {
+                std::cout << "unimplemented\n";
             }
+            else if (strcmp(argv[1], "test") == 0) {
+                std::cout << "Running Tests...\n";
+                Tests tests(argv[2], argv[3]);
+            }
+            else printUsageStatement(argv[0]);
         }
-        else if (strcmp(argv[1], "compile") == 0) {
-            std::cout << "unimplemented\n";
-        }
-        else printUsageStatement(argv[0]);
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
     }
 }
