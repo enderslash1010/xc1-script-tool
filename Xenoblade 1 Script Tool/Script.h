c0042b652fa0c41225c12eb6f789e940d17a17fa
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-10-05 13:13:01 -0500

    Removed offset attributes from Script class
    Offset attributes are only needed when initiating class from a .sb file, so they are moved into parameters for the .sb init functions

--- Xenoblade 1 Script Tool/Script.h
+++ Xenoblade 1 Script Tool/Script.h
@@ -8,9 +8,6 @@
 class Script
 {
 	unsigned char version, flags, isLoaded;
-	unsigned int codeOffset, IDPoolOffset, intPoolOffset, fixedPoolOffset, stringPoolOffset, functionPoolOffset;
-	unsigned int pluginImportsOffset, OCImportsOffset, functionImportsOffset;
-	unsigned int staticVariablesOffset, localPoolOffset, systemAttributePoolOffset, userAttributePoolOffset, debugSymbolsOffset;
 
 	bool littleEndian, is64Bit;
 
@@ -28,19 +25,19 @@ class Script
 	unsigned int getInteger4(unsigned char* memblock, int start);
 	float getFloat(unsigned char* memblock, int start);
 
-	void initCode(unsigned char* memblock);
-	void initIDPool(unsigned char* memblock);
-	void initIntPool(unsigned char* memblock);
-	void initFixedPool(unsigned char* memblock);
-	void initStringPool(unsigned char* memblock);
-	void initFunctionPool(unsigned char* memblock);
-	void initPluginImports(unsigned char* memblock);
-	void initOCImports(unsigned char* memblock);
-	void initFunctionImports(unsigned char* memblock);
-	void initStaticVariables(unsigned char* memblock);
-	void initLocalPool(unsigned char* memblock);
-	void initSystemAttributePool(unsigned char* memblock);
-	void initUserAttributePool(unsigned char* memblock);
+	void initCode(unsigned char* memblock, unsigned int codeOffset);
+	void initIDPool(unsigned char* memblock, unsigned int IDPoolOffset, unsigned int intPoolOffset);
+	void initIntPool(unsigned char* memblock, unsigned int intPoolOffset);
+	void initFixedPool(unsigned char* memblock, unsigned int fixedPoolOffset);
+	void initStringPool(unsigned char* memblock, unsigned int stringPoolOffset, unsigned int functionPoolOffset);
+	void initFunctionPool(unsigned char* memblock, unsigned int functionPoolOffset);
+	void initPluginImports(unsigned char* memblock, unsigned int pluginImportsOffset);
+	void initOCImports(unsigned char* memblock, unsigned int OCImportsOffset);
+	void initFunctionImports(unsigned char* memblock, unsigned int functionImportsOffset);
+	void initStaticVariables(unsigned char* memblock, unsigned int staticVariablesOffset);
+	void initLocalPool(unsigned char* memblock, unsigned int localPoolOffset);
+	void initSystemAttributePool(unsigned char* memblock, unsigned int systemAttributePoolOffset);
+	void initUserAttributePool(unsigned char* memblock, unsigned int userAttributePoolOffset);
 
 	void initCSVFunctionPool(std::vector<std::string> lines, int& currIndex);
 	void initCSVIDPool(std::vector<std::string> lines, int& currIndex);
